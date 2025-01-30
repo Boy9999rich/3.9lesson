4ba49910984dbfb2a1368d21fb67c09a0e5c0847
Boy9999rich <boymurodmamarajapov1@gmail.com>
2025-01-30 18:11:09 +0500

    upload

---
+++ 3.9lesson/3.9lesson/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _3._9lesson
+{
+    internal class Program
+    {
+        public delegate void Calculate(int otabekzor, int islomzor);
+        static void Main(string[] args)
+        {
+            //Calculate calculatde = Add;
+            //calculatde += multiply;
+            //calculatde += divide;
+            //calculatde += minus;
+
+            //calculatde.Invoke(10, 5);
+
+
+            Func<int, int, int> func = Add;
+            func += minus;
+            func.Invoke(15, 10);
+            
+            
+
+        }
+        public static int Add(int number, int nomer)
+        {
+            var res = number + nomer;
+            return res;
+        }
+        public static void multiply(int number, int nomer)
+        {
+            var res = number * nomer;
+            Console.WriteLine($"{res} is multiply result");
+        }
+        public static void divide(int number, int nomer)
+        {
+            var res = number / nomer;
+            Console.WriteLine($"{res} is divide result");
+        }
+        public static int minus(int number, int nomer)
+        {
+            var res = number - nomer;
+            return res;
+        }
+    }
+}
